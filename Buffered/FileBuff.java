b0c4d49fa73ae0b57b92a214dbf1a79fde869f6a
Roman <sup.makulin@mail.ru>
2023-02-10 13:02:55 +0300

    complete program: чтение строки с цифрами, выбор чисел, сумма и запись в файл

--- Buffered/FileBuff.java
+++ Buffered/FileBuff.java
@@ -1,26 +1,22 @@
 // Чтение и запись файла
 import java.io.*;
-
 public class FileBuff {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader buffR = new BufferedReader(new FileReader("File Read and Write/input.txt"));
-        String line = buffR.readLine();
-        String[] nums = line.split(" ");
-        int[] arrayNums = new int[nums.length];
-        int count = 0;
+        BufferedReader buffR = new BufferedReader(new FileReader("input.txt")); // Инициализация баффера
+        String line = buffR.readLine(); // считываем строку с файла
+        String[] nums = line.split(" "); // разделяем элементы строки в массив строк
         int sum = 0;
-        for (String number : nums) {
-            arrayNums[count] = Integer.parseInt(number);
-            sum += arrayNums[count];
-            count++;
-        }
-        System.out.println(sum);
-        buffR.close();
 
-        BufferedWriter buff = new BufferedWriter(new FileWriter("File Read and Write/output.txt"));
-        String result = Integer.toString(sum);
-        buff.write(result);
+        for (String i : nums) { // Считываем каждый элемент массива строки
+            if (i.matches("\\d+") == true) // Проверяем является ли элемент числом (d)
+                sum += Integer.parseInt(i); // Суммируем числа
+        }
+        buffR.close(); // Закрываем поток
+
+        BufferedWriter buff = new BufferedWriter(new FileWriter("output.txt"));
+        String result = Integer.toString(sum); // Записываем число снова в строку
+        buff.write(result); // Проводим запись в файл
         buff.close();
     }
 }
