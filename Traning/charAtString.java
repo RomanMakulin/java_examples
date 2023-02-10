51fe016ec4421aa7e796e9eb1143c94982250b87
Roman <sup.makulin@mail.ru>
2023-02-10 13:04:33 +0300

    traning

---
+++ Traning/charAtString.java
@@ -0,0 +1,15 @@
+package Traning;
+// Проверка вхождения одной строки в другую
+public class charAtString {
+    public static void main(String[] args) {
+        String s = "ab";
+        String j = "aabbccdaaabd";
+        int count = 0;
+
+        for (int i = 0; i < j.length(); i++) {
+            if (s.charAt(0) == j.charAt(i)) count += 1;
+            if (s.charAt(1) == j.charAt(i)) count += 1;
+        }
+        System.out.printf("Количество вхождений ab в строчке j: %d", count);
+    }
+}
---
+++ Traning/stringNum.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class stringNum {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader buffR = new BufferedReader(new FileReader("input.txt")); // Инициализация баффера
+        String line = buffR.readLine(); // считываем строку с файла
+        String[] nums = line.split(" "); // разделяем элементы строки в массив строк
+        int sum = 0;
+
+        for (String i : nums) { // Считываем каждый элемент массива строки
+            if (i.matches("\\d+") == true) // Проверяем является ли элемент числом (d)
+                sum += Integer.parseInt(i); // Суммируем числа
+        }
+        buffR.close(); // Закрываем поток
+
+        BufferedWriter buff = new BufferedWriter(new FileWriter("output.txt"));
+        String result = Integer.toString(sum); // Записываем число снова в строку
+        buff.write(result); // Проводим запись в файл
+        buff.close();
+    }
+}
