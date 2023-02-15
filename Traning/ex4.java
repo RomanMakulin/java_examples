c12f543e07f246a318515df5e6db1095a9184106
Roman <sup.makulin@mail.ru>
2023-02-15 16:34:30 +0300

    бинарный поиск элемента массива

---
+++ Traning/ex4.java
@@ -0,0 +1,27 @@
+// Бинарный поиск элемента массива
+
+public class ex4 {
+    public static void main(String[] args) {
+
+        int[] array = { 2, 4, 1, 6, 2, 10 };
+        System.out.printf("Позиция искомого элемента: %d", binarSearch(array, 10));
+
+    }
+
+    private static int binarSearch(int[] array, int key) {
+        int firstIndex = 0;
+        int lastIndex = array.length;
+
+        while (firstIndex < lastIndex) {
+            int middleIndex = (firstIndex + lastIndex) / 2;
+
+            if (array[middleIndex] == key)
+                return middleIndex;
+            else if (key < array[middleIndex])
+                lastIndex = middleIndex - 1;
+            else if (key > array[middleIndex])
+                firstIndex = middleIndex + 1;
+        }
+        return -1;
+    }
+}
