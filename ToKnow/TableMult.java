945c8c9f9d82e573223acc6a95bfc5f9344b85b8
unknown <WayWayz>
2023-02-16 22:55:29 +0300

    добавил таблицу умножения

---
+++ ToKnow/TableMult.java
@@ -0,0 +1,14 @@
+// таблица умножения
+
+public class TableMult {
+    public static void main(String[] args) {
+
+        for (int i = 2; i < 10; i++) {
+            for (int j = 2; j < 10; j++) {
+                System.out.printf("%d * %d = %d\n", i, j, i * j);
+            }
+            System.out.println();
+        }
+
+    }
+}
