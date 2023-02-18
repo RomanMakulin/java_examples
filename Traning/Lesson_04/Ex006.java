7e4e770126ee063b19cb8aa12f502000c18b902f
unknown <WayWayz>
2023-02-18 13:50:32 +0300

    fixed

---
+++ Traning/Lesson_04/Ex006.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Ex006 {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        pq.add(24);
+        pq.add(124);
+        pq.add(64);
+        pq.add(2);
+        
+        System.out.println(pq);
+
+        var str = "20 + 30".split(" ");
+        for (int i = 0; i < str.length; i++) {
+            System.out.println(str[i]);
+        }
+
+    }
+}
