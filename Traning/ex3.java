7977b9a0f1e5b5e5e3ab23358dc478edc2e7c507
unknown <WayWayz>
2023-02-19 10:31:56 +0300

    написать программу, которая выдаёт набор команд, позволяющий число a превратить в число b

--- Traning/ex3.java
+++ Traning/ex3.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-
 // +На вход некоторому исполнителю подаётся два числа (a, b). У исполнителя есть две команды
 // - команда 1 (к1): увеличить а в с раз, а умножается на c
 // - команда 2 (к2): увеличить на d, к a прибавляется d
@@ -13,31 +11,21 @@ public class ex3 {
 
     public static void main(String[] args) {
 
-        String str = "";
-        System.out.println(AAAAA(2, 11, 2, 1, str));
+        command(2, 11, 2, 1, "");
 
     }
 
-    // private static int command(int x, int y) {
+    private static void command(int currentNum, int resultNum, int c, int d, String path) {
 
-    //     // if (x == y)
-    //     // return 1;
-    //     // if (x > y)
-    //     // return 0;
-    //     // else return command(x + 1, y) + command(x * 2, y);
-
-    // }
-
-    private static ArrayList<String> AAAAA(int a, int b, int c, int d, String e) {
-        ArrayList<String> arr = new ArrayList<>();
-        if (a == b) {
-            arr.add(e);
+        if (currentNum > resultNum)
+            return;
+        if (currentNum == resultNum) {
+            System.out.println(path);
+            return;
         }
-        if (a < b) {
-            arr.addAll(AAAAA(a + c, b, c, d, e + "1"));
-            arr.addAll(AAAAA(a * d, b, c, d, e + "2"));
-        }
-        return arr;
+        command(currentNum * c, resultNum, c, d, path + "K1");
+        command(currentNum + d, resultNum, c, d, path + "K2");
+
     }
 
 }
