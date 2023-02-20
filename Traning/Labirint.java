f985f1ff7f1d5034d3f6018d11d9565921731816
Roman <sup.makulin@mail.ru>
2023-02-20 10:08:50 +0300

    labirint maket

--- Traning/Labirint.java
+++ Traning/Labirint.java
@@ -2,24 +2,32 @@ public class Labirint {
     public static void main(String[] args) {
         int rows = 10;
         int columns = 10;
-        int[][] array = new int[rows][columns];
+        String[][] array = new String[rows][columns];
         FillArray(array, rows, columns);
 
     }
 
-    private static void FillArray(int[][] array, int rows, int columns) {
+    private static void FillArray(String[][] array, int rows, int columns) {
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
 
                 if (j == columns - 1 || i == 0 || i == columns - 1 || j == 0) {
-                    array[i][j] = 1;
+                    array[i][j] = "#";
                 } else {
-                    array[i][j] = 0;
+                    array[i][j] = "_";
                 }
             }
         }
 
-        array[4][3] = -1;
+        array[4][3] = "/";
+        array[4][4] = "/";
+        array[4][5] = "/";
+        array[5][5] = "/";
+        array[6][5] = "/";
+        array[7][2] = "/";
+        array[7][3] = "/";
+        array[7][4] = "/";
+        array[7][5] = "/";
 
         // строим стены
         for (int i = 0; i < rows; i++) {
