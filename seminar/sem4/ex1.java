0f2c2d58ead2b7169d94fe0f4ff1aa9ac0d5cedd
Roman <sup.makulin@mail.ru>
2023-02-20 09:27:27 +0300

    HeapSort - пирамидальная сортировка

--- seminar/sem4/ex1.java
+++ seminar/sem4/ex1.java
@@ -1,37 +1,55 @@
+// HeapSort - пирамидальная сортировка
 package seminar.sem4;
-
 import java.util.Arrays;
 
 public class ex1 {
-    public static void main(String[] args) {
-        int rows = 10;
-        int columns = 10;
-        int[][] array = new int[rows][columns];
-        FillArray(array, rows, columns);
 
+    public static void main(String[] args) {
+        int[] array = { 3, 2, 6, 7, 1, 234, 8, -5 };
+        heapSort(array);
+        System.out.println(Arrays.toString(array));
     }
 
-    private static void FillArray(int[][] array, int rows, int columns) {
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
+    private static void heapSort(int[] array) {
+        int size = array.length;
 
-                if (j == columns - 1 || i == 0 || i == columns - 1 || j == 0) {
-                    array[i][j] = 1;
-                } else {
-                    array[i][j] = 0;
-                }
-            }
+        // строим дерево
+        for (int i = size / 2 - 1; i >= 0; i--) {
+            heapify(array, i, size);
         }
 
-        array[4][3] = -1;
+        // sorting
+        for (int i = size - 1; i >= 0; i--) {
+            int temp = array[i];
+            array[i] = array[0];
+            array[0] = temp;
 
-        // строим стены
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                System.out.print(array[i][j] + "    ");
-            }
-            System.out.println();
+            heapify(array, 0, i);
+        }
+    }
+
+    // меняем структуру данных: родитель - ребенок
+    private static void heapify(int[] array, int i, int size) {
+
+        int left = i * 2 + 1; // левый ребенок
+        int right = i * 2 + 2; // правый ребенок
+        int largest = i; // родитель
+
+        if (left < size && array[left] > array[largest]) {
+            largest = left;
+        }
+        if (right < size && array[right] > array[largest]) {
+            largest = right;
         }
 
+        // Если кто то оказался больше, то меняем местами
+        if (i != largest) {
+            int temp = array[i];
+            array[i] = array[largest];
+            array[largest] = temp;
+
+            // largest уже кто то из детей, у которых тоже могут быть свои дети
+            heapify(array, largest, i);
+        }
     }
 }
