890c01a963aa9a94530b86e5885572aa3efcb90c
unknown <WayWayz>
2023-02-21 20:10:15 +0300

    в ступоре

--- Traning/labirintNew.java
+++ Traning/labirintNew.java
@@ -5,8 +5,8 @@ import java.util.Queue;
 public class labirintNew {
     public static void main(String[] args) {
 
-        int rows = 5;
-        int columns = 5;
+        int rows = 10;
+        int columns = 10;
         int[][] array = new int[rows][columns];
         FillArray(array, rows, columns);
 
@@ -16,7 +16,7 @@ public class labirintNew {
 
         // список ll
         ArrayList<Point2D> ll = new ArrayList<>();
-        Point2D a = new Point2D(1, 3);
+        Point2D a = new Point2D(3, 4);
         ll.add(a);
 
         // очередь qw
@@ -40,25 +40,23 @@ public class labirintNew {
             System.out.println();
         }
         // x = 1; y = 3
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                if (array[ll.get(0).x][ll.get(0).y] == 1) {
-                    // upper
-                    if (array[ll.get(0).x - 1][ll.get(0).y] == 0) {
-                        array[ll.get(0).x - 1][ll.get(0).y] += 1;
-                    }
-                    // right
-                    if (array[ll.get(0).x][ll.get(0).y + 1] == 0) {
-                        array[ll.get(0).x][ll.get(0).y + 1] += 1;
-                    }
-                    // undo
-                    if (array[ll.get(0).x + 1][ll.get(0).y] == 0) {
-                        array[ll.get(0).x + 1][ll.get(0).y] += 1;
-                    }
-                    // left
-                    if (array[ll.get(0).x][ll.get(0).y - 1] == 0) {
-                        array[ll.get(0).x][ll.get(0).y - 1] += 1;
-                    }
+        for (int i = 1; i < rows; i++) {
+            for (int j = 3; j < columns; j++) {
+                // upper
+                if (array[ll.get(0).x - 1][ll.get(0).y] == 0) {
+                    array[ll.get(0).x - 1][ll.get(0).y] += 1;
+                }
+                // right
+                if (array[ll.get(0).x][ll.get(0).y + 1] == 0) {
+                    array[ll.get(0).x][ll.get(0).y + 1] += 1;
+                }
+                // undo
+                if (array[ll.get(0).x + 1][ll.get(0).y] == 0) {
+                    array[ll.get(0).x + 1][ll.get(0).y] += 1;
+                }
+                // left
+                if (array[ll.get(0).x][ll.get(0).y - 1] == 0) {
+                    array[ll.get(0).x][ll.get(0).y - 1] += 1;
                 }
             }
 
