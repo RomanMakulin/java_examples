41a51bf89162d1abbe51ba6413be56aa10e28903
unknown <WayWayz>
2023-02-23 15:33:22 +0300

    работа с лабиринтом

--- Traning/Labirint.java
+++ Traning/Labirint.java
@@ -1,53 +1,74 @@
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ * Labirint
+ */
 public class Labirint {
+
     public static void main(String[] args) {
-        int rows = 15;
-        int columns = 15;
-        String[][] array = new String[rows][columns];
-        FillArray(array, rows, columns);
+        int rows = 8;
+        int columns = 8;
+        int[][] array = new int[rows][columns];
 
-    }
+        // печатаем массив изначальный
+        
 
-    private static void FillArray(String[][] array, int rows, int columns) {
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
+        int x = 4;
+        int y = 4;
+        Queue<Point2D> qu = new LinkedList<>();
+        Point2D a = new Point2D(x, y);
+        qu.add(a);
 
-                if (j == columns - 1 || i == 0 || i == columns - 1 || j == 0) {
-                    array[i][j] = "#";
-                } else {
-                    array[i][j] = "_";
-                }
+        while (array.length > 0) {
+
+            // curr
+            if (array[qu.peek().x][qu.peek().y] == 0) {
+                array[a.x][a.y] += 1;
+            }
+
+            // up
+            if (array[qu.peek().x-1][qu.peek().y] == 0) {
+                a = new Point2D(x-1, y);
+                qu.add(a);
+                qu.remove();
+                array[a.x][a.y] += 1;
+            }
+
+            // right
+            if (array[qu.peek().x][qu.peek().y+1] == 0) {
+                array[a.x][a.y+1] += 1;
+            }
+
+            // down
+            if (array[qu.peek().x+1][qu.peek().y] == 0) {
+                array[a.x+1][a.y] += 1;
+            }
+
+            // left
+            if (array[qu.peek().x][qu.peek().y-1] == 0) {
+                array[a.x][a.y-1] += 1;
+                break;
             }
         }
 
-        array[4][3] = "/";
-        array[4][4] = "/";
-        array[4][5] = "/";
-        array[7][4] = "1";
-        array[7][5] = "/";
-        array[8][5] = "/";
-        array[9][5] = "/";
-        array[5][5] = "/";
-        array[6][5] = "/";
-        array[10][2] = "/";
-        array[10][3] = "/";
-        array[10][4] = "/";
-        array[10][5] = "/";
-
-        // строим стены
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                System.out.print(array[i][j] + "    ");
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                System.out.print(array[i][j] + "  ");
             }
             System.out.println();
         }
 
-        Queue<Integer> ll = new LinkedList<>();
-        ll.add(2);
+    }
+}
 
+class Point2D {
+    int x;
+    int y;
+
+    public Point2D(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
 }
